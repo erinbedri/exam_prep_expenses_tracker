cd6cf6583fae5ff1455af82c2f1d6f86fa81bf6e
erinbedri <erinbedri@gmail.com>
2022-09-23 16:52:43 +0200

    created superuser and registered models to admin

--- exam_prep_expenses_tracker/web/admin.py
+++ exam_prep_expenses_tracker/web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from exam_prep_expenses_tracker.web.models import Profile, Expense
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'budget')
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'price')
+
